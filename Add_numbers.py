1c42ffd8417fe04ed677fe5e3efd97d77d08cf68
abhinavcool747 <abhinav747@outlook.in>
2024-06-05 17:57:23 +0530

    Added second change

--- Add_numbers.py
+++ Add_numbers.py
@@ -2,6 +2,6 @@ def add_two_numbers(num1, num2):
     return num1 + num2
 
 number1 = 12.7
-number2 = 7.3
+number2 = 8.3
 result = add_two_numbers(number1, number2)
 print(f'The sum of {number1} and {number2} is {result}')
